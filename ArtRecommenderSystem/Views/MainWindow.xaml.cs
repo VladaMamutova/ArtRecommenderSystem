c92a58bcd85354ab8eb9f4e5641568d7331351a4
Vlada Mamutova <36194644+VladaMamutova@users.noreply.github.com>
2020-12-10 21:26:42 +0300

    feat: add menu and pages

---
+++ ArtRecommenderSystem/Views/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ArtRecommenderSystem.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainPage mainPage;
+        private RecommendationPage recommendationPage;
+        private MyGalleryPage myGalleryPage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainPage = new MainPage();
+            recommendationPage = new RecommendationPage();
+            myGalleryPage = new MyGalleryPage();
+            MainRadioButton.IsChecked = true;
+        }
+
+        private void Move(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ButtonClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ButtonExpand_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+                ((Button)sender).ToolTip = "Развернуть";
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+                ((Button)sender).ToolTip = "Свернуть";
+            }
+        }
+
+        private void MainPage_OnChecked(object sender, RoutedEventArgs e)
+        {
+            MyGalleryRadioButton.IsChecked = false;
+            ContentFrame.Navigate(mainPage);
+        }
+
+        private void RecommendationPage_OnChecked(object sender, RoutedEventArgs e)
+        {
+            MyGalleryRadioButton.IsChecked = false;
+            ContentFrame.Navigate(recommendationPage);
+        }
+
+        private void MyGalleryPage_OnChecked(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(myGalleryPage);
+        }
+    }
+}
--- ArtRecommenderSystem/Views/MyGalleryPage.xaml.cs
+++ ArtRecommenderSystem/Views/MyGalleryPage.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ArtRecommenderSystem
+namespace ArtRecommenderSystem.Views
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MyGalleryPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MyGalleryPage : Page
     {
-        public MainWindow()
+        public MyGalleryPage()
         {
             InitializeComponent();
         }
